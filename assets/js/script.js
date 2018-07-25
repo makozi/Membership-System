d13beb2261e695da948c985f2f9ade1fbc6a3a79
[Makozi] <[marizumakozi97@gmail.com]>
2018-07-25 17:26:57 +0100

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
